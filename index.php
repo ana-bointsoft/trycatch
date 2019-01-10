bd23f21e8ac0b5cb6737578c327f40bdf7b22cbe
ana-bointsoft <45634467+ana-bointsoft@users.noreply.github.com>
2019-01-10 13:37:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,49 @@
-<?php 
 
- header('Location: /trycatch/csvmanager/list.php');
-    exit;
+<html>
+<head>
+<style>
+table, tr, th, td{
+  border: 2px solid black;
+   padding: 15px;
+	border-spacing: 5px; 
+    border-collapse: collapse;
+}
+th, tr{
+	text-align: center;
+	colspan= 
+}
+td{ 
+	text-align: left;  
+} 
+</style>
+</head>
+<body>
+
+<h2>Bordered Table</h2>
+<p>Use the CSS border property to add a border to the table.</p>
+
+<table style="width:85%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+  <tr>
+    <td>John</td>
+    <td>Doe</td>
+    <td>80</td>
+  </tr>
+</table>
+
+</body>
+</html>
